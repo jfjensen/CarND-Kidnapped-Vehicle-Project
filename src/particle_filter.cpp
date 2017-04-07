e17dc032e6e468b07d22ff022b912ab60685e0f2
Jes Fink-Jensen <jes@finkjensen.be>
2017-04-07 16:57:33 +0200

    Removed comments

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -20,7 +20,7 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
 	// Add random Gaussian noise to each particle.
 	// NOTE: Consult particle_filter.h for more information about this method (and others in this file).
 	num_particles = 100;
-	//particles.resize(num_particles);
+
 	weights.resize(num_particles);
 
 	random_device rd;
@@ -46,11 +46,7 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
 
 		particles.push_back(new_particle);
 
-		// cout << "Sample " << i + 1 << " " 
-		// 	<< new_particle.x << " " 
-		// 	<< new_particle.y << " " 
-		// 	<< new_particle.theta << " " 
-		// 	<< new_particle.weight << endl;
+
 	}
 
 	is_initialized = true;
@@ -65,9 +61,6 @@ void ParticleFilter::prediction(double delta_t, double std_pos[], double velocit
 
 	for (int i = 0; i < num_particles; i++)
 	{
-
-		// Particle current_particle;
-		// current_particle = particles[i];
 
 		Particle current_particle = particles[i];
 
@@ -101,28 +94,14 @@ void ParticleFilter::prediction(double delta_t, double std_pos[], double velocit
 		double std_y     = std_pos[1];
 		double std_theta = std_pos[2];
 
-		// normal_distribution<double> dist_x(x_f, std_x);
-		// normal_distribution<double> dist_y(y_f, std_y);
-
 		normal_distribution<double> dist_x(0.0, std_x);
 		normal_distribution<double> dist_y(0.0, std_y);
 		normal_distribution<double> dist_theta(0.0, std_theta);
 		
-		// particles[i].x = dist_x(gen);
-		// particles[i].y = dist_y(gen);
-		// particles[i].theta = theta_f;
-
-		// current_particle.x = dist_x(gen);
-		// current_particle.y = dist_y(gen);
 		current_particle.x = x_f + dist_x(gen);
 		current_particle.y = y_f + dist_y(gen);
 		current_particle.theta = theta_f + dist_theta(gen);
 
-		// cout << "Sample " << i + 1 << " " 
-		// 	<< current_particle.x << " " 
-		// 	<< current_particle.y << " " 
-		// 	<< current_particle.theta << " " 
-		// 	<< current_particle.weight << endl;
 		particles[i] = current_particle;
 	}
 
@@ -150,8 +129,6 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
 	//   for the fact that the map's y-axis actually points downwards.)
 	//   http://planning.cs.uiuc.edu/node99.html
 
-	// cout << "std_landmark: " << std_landmark[0] << " " << std_landmark[1] << endl;
-
 	double std_x = std_landmark[0];
 	double std_y = std_landmark[1];
 
@@ -167,29 +144,15 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
 		x_f     = current_particle.x;
 		y_f     = current_particle.y;
 		theta_f = current_particle.theta;
-		// weight  = current_particle.weight;
 		weight  = 1.0;
-
-		// cout << "Particle: " << x_f << " " << y_f << endl;
 
 		for (int i = 0; i < observations.size(); i++)
 		{
 			LandmarkObs obs = observations[i];
 			double obs_x, obs_y;
 
-			// obs_x = x_f + (obs.x * cos(theta_f)) - (obs.y * sin(theta_f));
-			// obs_y = y_f + (obs.x * sin(theta_f)) + (obs.y * cos(theta_f));
-
 			obs_x = x_f + (obs.x * cos(theta_f)) - (obs.y * sin(theta_f));
 			obs_y = y_f + (obs.x * sin(theta_f)) + (obs.y * cos(theta_f));
-
-			// obs_x = x_f + obs.x;
-			// obs_y = y_f + obs.y;
-
-			// obs_x = obs.x + (x_f * cos(theta_f)) - (y_f * sin(theta_f));
-			// obs_y = obs.y + (x_f * sin(theta_f)) + (y_f * cos(theta_f));
-
-			// cout << "Particle: " << n << " w: " << weight << " Landmark: " << obs_x << " " << obs_y ;//<< endl;
 
 			vector<double> ranges;
 
@@ -203,16 +166,13 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
 				double y_range = map_y - obs_y;
 
 				double range    = sqrt(x_range*x_range + y_range*y_range);
-				// cout << dist <<" "; 
-				// double range   = ((x_range*x_range)/(std_x*std_x)) + ((y_range*y_range)/(std_y*std_y));
 
 				ranges.push_back(range);
 			}
 
 			auto min_range = min_element(begin(ranges),end(ranges));
 			int ix = distance(begin(ranges), min_range);
-			// cout << *min_range << " ";
-
+	
 			Map::single_landmark_s closest_landmark = map_landmarks.landmark_list[ix];
 
 			double map_x = closest_landmark.x_f;
@@ -223,17 +183,10 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
 
 			double term   = ((x_range*x_range)/(std_x*std_x)) + ((y_range*y_range)/(std_y*std_y));
 
-			// sort(ranges.begin(), ranges.end());
-			// cout << "  range: " << ranges[0] << endl;
-			// double min_range = ranges[0];
-			// if (min_range>sensor_range)
-			// 	min_range = sensor_range;
-
-			weight *= (exp(-0.5*term))/(2*M_PI*std_x*std_y); // seriously??
+			weight *= (exp(-0.5*term))/(2*M_PI*std_x*std_y); 
 			
 
 		}
-		// cout << endl;
 
 		weight += 1e-300;
 		current_particle.weight = weight;
@@ -254,7 +207,7 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
 
 		weights[n] = current_particle.weight;
 
-		// cout << "weight n:" << n << " " << weights[n] << endl;
+
 	}
 
 }
@@ -277,8 +230,6 @@ void ParticleFilter::resample() {
 	// https://stackoverflow.com/questions/10158756/using-stdmax-element-on-a-vectordouble
 	double mw = *max_element(begin(particle_weights), end(particle_weights));
 
-	// cout << "Indexes resampled: ";
-
 	for (int n = 0; n < num_particles; n++)
 	{
 		// http://www.cplusplus.com/reference/random/uniform_real_distribution/
@@ -290,12 +241,11 @@ void ParticleFilter::resample() {
 			beta -= particle_weights[index];
 			index = (index + 1) % num_particles;
 		}
-		// cout << index << " ";
 		Particle particle_copy = particles[index];
 
 		resampled_particles.push_back(particle_copy);
 	}
-	// cout << endl;
+
 	particles = resampled_particles;
 
 }
